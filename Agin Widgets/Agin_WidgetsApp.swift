a8eae49849d3e21fd3c6c1c3f8abb8a7815e1885
Tymek <vonautymek@gmail.com>
2025-05-21 11:04:32 +0200

    Initial Commit

---
+++ Agin Widgets/Agin_WidgetsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agin_WidgetsApp.swift
+//  Agin Widgets
+//
+//  Created by Tymek on 21/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Agin_WidgetsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
